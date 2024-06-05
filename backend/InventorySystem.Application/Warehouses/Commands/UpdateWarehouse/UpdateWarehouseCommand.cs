ac92188e82f76ff3f42c78463ac0df10a0d6445a
bolanosjosue <josuebolanos2004@gmail.com>
2024-06-05 08:16:19 -0600

    feat(suppliers,warehouses): add update operations

---
+++ backend/InventorySystem.Application/Warehouses/Commands/UpdateWarehouse/UpdateWarehouseCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem.Application.Warehouses.Commands.UpdateWarehouse
+{
+    internal class UpdateWarehouseCommand
+    {
+    }
+}
